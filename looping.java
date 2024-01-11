d0e6320c483417f104b0acb3131f3146c491f306
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-01-11 15:40:11 +0530

    counting

---
+++ looping.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class looping {
+    public static void main(String[] args) {
+
+        System.out.println("entered the number .");
+        Scanner sc  = new Scanner(System.in);
+
+      int  n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+
+
+
+        }
+
+    }
+
+
