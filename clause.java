d0fb199758ee9fb8ee78f788aa4073c73a8f920f
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-01-11 14:29:23 +0530

    clause

---
+++ clause.java
@@ -0,0 +1,13 @@
+public class clause {
+    public static void main(String[] args) {
+        boolean learning = true ;
+
+        if (learning) {
+            System.out.println("java programmer .");
+
+            }
+        else{
+            System.out.println("whta are you doing here .!");
+        }
+    }
+}
