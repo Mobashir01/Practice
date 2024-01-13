a725060de748807ce194fe36b3a0e490bfd8262c
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-01-13 19:54:38 +0530

    star*

---
+++ starr.java
@@ -0,0 +1,12 @@
+public class starr {
+    public static void main(String[] args) {
+
+        int row, numberofstar;
+        for (row = 1; row <= 10; row++) {
+            for (numberofstar = 1; numberofstar <= 10; numberofstar++) {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
