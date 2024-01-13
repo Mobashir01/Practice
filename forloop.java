6df8f909cfd60703c70b0477411822f9a412aae0
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-01-13 19:20:50 +0530

    simple loop

---
+++ forloop.java
@@ -0,0 +1,10 @@
+public class forloop {
+    public static void main(String[] args) {
+
+         int c;
+
+         for (c=1; c <= 10; c++){
+             System.out.println(c);
+         }
+    }
+}
