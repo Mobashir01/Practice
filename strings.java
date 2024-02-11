282aab72ddc0f3512e988aa2806490c920e5e52e
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-02-11 23:42:43 +0530

    cansolestar

---
+++ strings.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class strings {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder ("moby");
+        System.out.println(sb);
+    }
+}
