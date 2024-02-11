1b1c5466aaddd157fe705d03c9ea0015b84db683
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-02-12 02:14:04 +0530

    polestar

---
+++ stringsbuilderr.java
@@ -0,0 +1,35 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class stringsbuilderr {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("computer");
+        System.out.println(sb);
+
+        //indexing of strig
+        System.out.println(sb.charAt(3));
+
+        // set char at string
+       // System.out.println(sb.setCharAt(3, 'm'());
+        sb.charAt(3);
+        System.out.println(sb);
+
+        // char insert in string
+        System.out.println(sb.insert(3, 'p'));
+
+        //char delete in string
+        sb.delete(2,3);
+        System.out.println(sb);
+
+        //new string add
+        sb.append("science");
+        System.out.println(sb);
+
+        //char length of string
+        System.out.println(sb.length());
+    }
+}
+
+
