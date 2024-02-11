dda0ca3ed7ef73ac333877d89a4afc362d4faafe
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-02-11 20:01:40 +0530

    consolestar

---
+++ arrray.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class arrray {
+    public static void main(String[] args) {
+
+        int[] marks = new int [3];
+
+        marks [0] = 97; //physics
+        marks [1] = 95; //chemistry
+        marks [2] = 90; //maths
+
+        for(int i = 0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
