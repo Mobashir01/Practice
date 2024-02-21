c1b7d8e8abe654d75c3d25fc2beeacd7a2269277
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-02-21 07:52:32 +0530

    adding

---
+++ aaddiinngggg.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class aaddiinngggg {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in );
+
+        System.out.println("enter the value of 1st number ");
+        int number1=
+        sc.nextInt();
+
+        System.out.println("enter the 2nd number ");
+        int number2=
+        sc.nextInt();
+
+        //add two number ;
+
+        int sum= number1 + number2;
+
+        System.out.println("the sum of " + number1 + "and " +number2+ "is " +sum);
+
+
+
+
+
+
+
+    }
+}
