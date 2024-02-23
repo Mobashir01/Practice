a5a7374f6fc49872dbc5132dc56ec1ebf96ae08e
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-02-24 04:57:48 +0530

    kkkk

---
+++ kkkkkkk.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class kkkkkkk {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // taking input;
+        int a;
+        int b;
+        sc.nextInt();
+        System.out.println("enter the sum of two number");
+
+        for(int i = 0; i<=10; i++){
+            System.out.println("enter the sum a+b");
+            sc.nextInt();
+        }
+    }
+}
