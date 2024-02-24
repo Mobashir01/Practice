2c2f62c8d2baf4acdb92226b66063b2f893154cc
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-02-25 04:18:14 +0530

    loop

---
+++ arrrray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class arrrray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+     //   int arr = new arr[](System.in);
+        int a=5;
+        int b=10;
+        int sum;
+        System.out.println("enter the starting number or initialise the number where from start the loop");
+
+        sc.nextInt();
+
+        for(int i =0; i <=15; i++){
+            System.out.println(i);
+
+          //  public static void printarray()
+              //  if(n==0){
+              //      return;
+                }
+            }
+        }
+
