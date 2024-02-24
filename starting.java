f38db1fda0c65e2fefe7be88fa44e0e659b00af9
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-02-25 04:28:25 +0530

    simple

---
+++ starting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class starting {
+    public static void main(String[] args) {
+
+        int a,b;
+        int sum ;
+
+        Scanner sc = new Scanner(System.in);
+
+        // taking input from user;
+
+
+        sc.nextInt();
+        System.out.println("enter the value of number  a + b ");
+
+      //  sc.nextInt();
+
+    }
+}
