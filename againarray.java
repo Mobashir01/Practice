7ab20b022bb3cd9e7f5fab6e1d2097348d6a2acb
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-02-26 02:49:10 +0530

    simple

---
+++ againarray.java
@@ -0,0 +1,12 @@
+public class againarray {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[]{20,40,60};
+
+        for(int i=0; i<a.length; i++){
+
+            System.out.println(a[i]);
+        }
+    }
+}
