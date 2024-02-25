bf3daf9ca8dd61b8c9d9f61c9350af6d8102d7f1
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-02-26 02:42:39 +0530

    simple

---
+++ simpleprogofarry.java
@@ -0,0 +1,9 @@
+public class simpleprogofarry {
+    public static void main(String[] args) {
+
+        int[] a = new int []{10,20,30};
+        for(int i = 0; i<3;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
