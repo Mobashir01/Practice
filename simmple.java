d2176aae237e954ee3c16dac60de9821975c4c4b
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-02-27 01:26:13 +0530

    Adding

---
+++ simmple.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class simmple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the first number ");
+
+        int a=
+        sc.nextInt();
+        System.out.println("enter the second number ");
+
+        int b=
+
+        sc.nextInt();
+
+        int sum = a +b;
+
+
+        System.out.println("sum of"+a+"and"+b+"is"+sum);
+    }
+}
