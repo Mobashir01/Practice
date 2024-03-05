4e86370babdb3f0635a53c5a2e62e6d04b8efaae
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-03-05 12:40:37 +0530

    greater of two

---
+++ greateroftwo.java
@@ -0,0 +1,14 @@
+public class greateroftwo {
+    public static void main(String[] args) {
+
+        int a = 130, b = 20;
+
+        if(a>b){
+            System.out.println("a is greater then b ");
+
+        }
+        else {
+            System.out.println("b is greater then a");
+        }
+    }
+}
