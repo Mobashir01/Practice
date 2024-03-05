606abc6ef655165a105f3e938123b1896e7ebdf8
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-03-05 13:00:17 +0530

    greater of three

---
+++ greaterofthree.java
@@ -0,0 +1,18 @@
+public class greaterofthree {
+    public static void main(String[] args) {
+
+        int a = 1000, b = 2000, c = 300;
+
+        if(a>b && a>c){
+            System.out.println("a is greater then both b and c ");
+
+        }
+        else if (b>a && b>c){
+            System.out.println("b is greater then both a and c ");
+
+        }
+        else{
+            System.out.println("c is greater ");
+        }
+    }
+}
