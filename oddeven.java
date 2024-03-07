c356e892167600d7ede656f4beedea506597765a
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-03-07 22:26:59 +0530

    oddeven

---
+++ oddeven.java
@@ -0,0 +1,14 @@
+public class oddeven {
+    public static void main(String[] args) {
+
+        int no = 91;
+
+        if(91 % 2 == 0){
+            System.out.println("number is even ");
+
+        }
+        else{
+            System.out.println("number is odd ");
+        }
+    }
+}
