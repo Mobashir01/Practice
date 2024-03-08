f7aaf5c9e3a831695e83e1c0fa194d95babe20de
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-03-08 23:55:41 +0530

    arrayloop

---
+++ llllooping.java
@@ -0,0 +1,9 @@
+public class llllooping {
+    public static void main(String[] args) {
+
+        int[] a = {8,3,4,2,5,1};
+        for(int i=0; i< a.length; i++){
+            System.out.println(i);
+        }
+    }
+}
