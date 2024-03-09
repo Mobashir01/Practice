14ba5cfb6c23f18227d902fc1637327c40d5db59
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-03-09 17:08:04 +0530

    tableeee

---
+++ tableeee.java
@@ -0,0 +1,10 @@
+public class tableeee {
+    public static void main(String[] args) {
+
+        int[] a = new int[10];
+
+        for(int i = 1; i<=a.length; i++){
+            System.out.println(i*10);
+        }
+    }
+}
