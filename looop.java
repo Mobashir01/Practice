07c0cd3305f2f7fab8b89044c90143436b40730e
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-03-10 20:33:35 +0530

    loop

---
+++ looop.java
@@ -0,0 +1,9 @@
+public class looop {
+    public static void main(String[] args) {
+
+        int[] a = new int[10];
+        for(int i =0; i<10; i++){
+            System.out.println(i);
+        }
+    }
+}
