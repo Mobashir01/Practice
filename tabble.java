9adb0909af7a3f37fc48b41f1306377d2c0cdd8c
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-03-11 16:06:48 +0530

    table

---
+++ tabble.java
@@ -0,0 +1,9 @@
+public class tabble {
+    public static void main(String[] args) {
+
+        int t = 2;
+        for(int i = 1; i<=10;i++){
+            System.out.println(t*i);
+        }
+    }
+}
