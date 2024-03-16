9ae5905843c1b23df9f82501b4b0eacda00fa134
Mobashir01 <mobashirkhanmrk@gmail.com>
2024-03-17 02:04:29 +0530

    array

---
+++ againbubblesort.java
@@ -0,0 +1,22 @@
+public class againbubblesort {
+    public static void main(String[] args) {
+
+        int[] a = {9,8,7,6,5,4,3,2,1};
+        int temp;
+
+        for(int i = 0; i<a.length; i++){
+            for(int j = 0; j<a.length-1; j++){
+
+                if( a[j] > a[j+1]){
+
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        for(int i = 0; i<a.length; i++){
+            System.out.println(a[i]+ " ");
+        }
+    }
+}
